66ca9f98c2d897d45b0d373ccf04b86d321b3e33
Ralph Meijer <ralphm@ik.nu>
2015-06-10 16:30:50 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dnsq',
-      version='1.1.3',
+      version='1.1.4',
       description='DNS Query Tool',
       long_description=open("README.rst").read(),
       author='Rackspace',
